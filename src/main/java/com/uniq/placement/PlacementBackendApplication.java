5848088c48fc9739deeb70ba55684937ff436a60
Indhuja <indhuja.vijay@finytive.com>
2026-09-14 16:51:57 +0530

    Timezone bug fix

--- src/main/java/com/uniq/placement/PlacementBackendApplication.java
+++ src/main/java/com/uniq/placement/PlacementBackendApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 public class PlacementBackendApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(PlacementBackendApplication.class, args);
     }
 }
